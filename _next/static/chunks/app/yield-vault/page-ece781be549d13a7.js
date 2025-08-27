5438ee9e7399332f8b16746089cfbd3524bcc3af
nonolooklook <nonolooklook@users.noreply.github.com>
2025-08-27 06:30:11 +0000

    deploy: wandfi/wand-apps@94236e63b92fe201d15b2571c32a99d4f3cd8b22

---
+++ _next/static/chunks/app/yield-vault/page-ece781be549d13a7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1358],{24297:function(n,e,u){Promise.resolve().then(u.bind(u,15942))}},function(n){n.O(0,[2257,7495,9891,6358,4345,5097,8109,2366,1900,2409,6102,12,2653,5631,6056,742,278,1838,3956,5935,3559,5702,2847,5942,4195,6338,1744],function(){return n(n.s=24297)}),_N_E=n.O()}]);
